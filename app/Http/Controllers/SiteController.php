c54e48455ab6f93bbe87fbd216f195df7be94827
Ruslan-cmd <ruslantelenkov@mail.ru>
2020-08-19 19:14:56 +0700

    default

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class SiteController extends Controller
 {
     public function index (){
-        return view('main');
+        return view('layouts/main');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get( '/', 'SiteController@index');
 
+/*
 Route::get('/', function () {
     return view('layouts/main');
 });
+*/
