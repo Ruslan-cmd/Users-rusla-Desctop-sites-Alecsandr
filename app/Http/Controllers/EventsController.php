7d90a67a131dd3be55033f59e1d7c892cd19cdaf
Ruslan-cmd <ruslantelenkov@mail.ru>
2020-10-19 10:11:30 +0700

    default

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers;
 use App\Event;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Pagination\Paginator;
 class EventsController extends Controller
 {
     public function index()
     {
         return view('layouts/events', [
             'events' => $this->getEvent(),
-
+            'events1' => $this->getEvent1(),
         ]);
     }
 private function getEvent(){
@@ -20,7 +20,18 @@ private function getEvent(){
         ->where('date', '>=', now())
            ->orderByDesc('date')
         ->get();
+
 }
+    private function getEvent1()
+    {
+      return  $event = Event::query()
+            ->where('date', '<=', now())
+            ->paginate(4);
+
+    }
+
+
+
 }
 
 
--- resources/views/layouts/events.blade.php
+++ resources/views/layouts/events.blade.php
@@ -54,11 +54,11 @@
         </div>
     </div>
 </header>
-<p style="font-size:4em">Предстоящие события</p>
+
 <main role="main">
     <div class="album py-5 bg-light">
         <div class="container">
-
+            <p style="font-size:4em">Предстоящие события</p>
             <div class="row">
                 @foreach($events as $event)
 
@@ -82,6 +82,32 @@
                 </div>
  @endforeach
             </div>
+            <p style="font-size:4em">События, которые прошли</p>
+            <div class="row">
+                @foreach($events1 as $event)
+
+                    <div class="col-md-4">
+                        <div class="card mb-4 box-shadow">
+                            <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                            <div class="card-body">
+                                <p class="card-text">{{$event->title}}</p>
+                                <p class="card-text">Организатор мероприятия: {{$event->organizer}}</p>
+                                <p class="card-text">Дата события: {{$event->date}}</p>
+                                <p class="card-text">Стоимость: {{$event->price}} рублей</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                    </div>
+                                    <small class="text-muted">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+
+            </div>
+            {{($events1->render())}}
         </div>
     </div>
 
