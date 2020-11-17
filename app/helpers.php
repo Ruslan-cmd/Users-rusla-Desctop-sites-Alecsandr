c5644098a9adfd0d269efc13f9bf3d50f0b99e01
Ruslan-cmd <ruslantelenkov@mail.ru>
2020-11-17 11:56:04 +0700

    default

--- app/helpers.php
+++ app/helpers.php
@@ -1,10 +1,11 @@
 <?php
+use app\Cart;
 if (!function_exists('cart')){
     /**
      * @return \App\Dish
      */
     function cart() {
-        return app()->make(\App\Cart::class);
-    //return app(Cart::class);
+        //return app()->make(\App\Cart::class);
+    return app(Cart::class);
     }
 }
